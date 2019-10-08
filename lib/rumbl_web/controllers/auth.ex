2991f623257d1d920e599b0bfd2cbecafd1f90d1
Somers Matthews <somersbmatthews@gmail.com>
2019-10-08 11:46:11 -0500

    at point of compile error with import with :router

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,5 +1,6 @@
 defmodule RumblWeb.Auth do
    import Plug.Conn
+   import Phoenix.Controller
 
    def init(opts), do: opts
 
@@ -14,5 +15,20 @@ defmodule RumblWeb.Auth do
       |> assign(:current_user, user)
       |> put_session(:user_id, user.id)
       |> configure_session(renew: true)
+   end
+
+   def logout(conn) do
+      configure_session(conn, drop: true)
+   end
+
+   def authenticate_user(conn, _opts) do
+      if conn.assigns.current_user do
+         conn
+      else
+         conn
+         |> put_flash(:error, "You must be logged in to access that page")
+         |> redirect(to: Routes.page_path(conn, :index))
+         |> halt()
+      end
    end
 end
