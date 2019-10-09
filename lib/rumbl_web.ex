d40627fa481a7c929325f7d590b8b6db11f01d05
Somers Matthews <somersbmatthews@gmail.com>
2019-10-09 05:19:11 -0500

    at duplicate category name problem

--- lib/rumbl_web.ex
+++ lib/rumbl_web.ex
@@ -23,7 +23,7 @@ defmodule RumblWeb do
 
       import Plug.Conn
       import RumblWeb.Gettext
-      import RumblWeb.Auth, :only [authenticate_user: 2]
+      import RumblWeb.Auth, only: [authenticate_user: 2]
       alias RumblWeb.Router.Helpers, as: Routes
     end
   end
@@ -51,7 +51,7 @@ defmodule RumblWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
-      import RumblWeb.Auth, :only [authenticate_user: 2]
+      import RumblWeb.Auth, only: [authenticate_user: 2]
     end
   end
 
